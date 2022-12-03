1b2f3535ac9db62de4dcbe25b5fc95fc3eab3b30
Francesco <56157402+BrunoFrancesco97@users.noreply.github.com>
2022-12-03 15:37:24 +0100

    Delete Task 3/app directory

--- Task 3/app/android/app/src/main/kotlin/dev/flutter/navigation_and_routing/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package gq.hackerrank
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- Task 3/app/lib/src/data.dart
+++
@@ -1,8 +0,0 @@
-// Copyright 2021, the Flutter project authors. Please see the AUTHORS file
-// for details. All rights reserved. Use of this source code is governed by a
-// BSD-style license that can be found in the LICENSE file.
-
-export 'data/course.dart';
-export 'data/assignment.dart';
-export 'data/library.dart';
-export 'data/constants.dart';
